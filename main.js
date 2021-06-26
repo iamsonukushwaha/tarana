85c314889b47452f697ee914a8bddfe12c240c31
Nihal Priyadarshi <68635556+Nihal-Priyadarshi@users.noreply.github.com>
2021-06-26 21:18:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,197 @@
+let previous = document.querySelector('#pre');
+let play = document.querySelector('#play');
+let next = document.querySelector('#next');
+let title = document.querySelector('#title');
+let recent_volume= document.querySelector('#volume');
+let volume_show = document.querySelector('#volume_show');
+let slider = document.querySelector('#duration_slider');
+let show_duration = document.querySelector('#show_duration');
+let track_image = document.querySelector('#track_image');
+let auto_play = document.querySelector('#auto');
+let present = document.querySelector('#present');
+let total = document.querySelector('#total');
+let artist = document.querySelector('#artist');
+
+
+let timer;
+let autoplay = 0;
+
+let index_no = 0;
+let Playing_song = false;
+
+//creating an audio Element.
+let track = document.createElement('audio');
+
+
+//All songs list.
+let All_song = [
+	{
+	  name: "Aaj Phir",
+	  path: "music/Aaj_Phir.mp3",
+	  img: "img/img1.jpg",
+	  singer: "Arijit Singh & Samira Koppikar"
+	},
+	{
+	 name: "Baarish",
+	 path: "music/Baarish.mp3",
+	 img: "img/img2.jpg",
+	 singer: "Tanishk Bagchi, Shashaa Tirupati"
+	},
+	{
+	  name: "Bolna",
+	  path: "music/bolna.mp3",
+	  img: "img/img3.jpg",
+	  singer: "Tanishk Bagchi, Arijit Singh & Asees Kaur"
+	},
+	{
+	  name: "Hawayein",
+	  path: "music/Hawayein.mp3",
+	  img: "img/img4.jpg",
+	  singer: "Arijit Singh , Pritam"
+	},
+	{
+	  name: "Kaun Tujhe",
+	  path: "music/Kaun_Tujhe.mp3",
+	  img: "img/img5.jpg",
+	  singer: "Palak Muchhal"
+	}
+];
+
+
+// Functions Starts Here 
+
+
+// function load the track
+function load_track(index_no){
+	clearInterval(timer);
+	reset_slider();
+
+	track.src = All_song[index_no].path;
+	title.innerHTML = All_song[index_no].name;	
+	track_image.src = All_song[index_no].img;
+    artist.innerHTML = All_song[index_no].singer;
+    track.load();
+
+	timer = setInterval(range_slider ,1000);
+	total.innerHTML = All_song.length;
+	present.innerHTML = index_no + 1;
+}
+
+load_track(index_no);
+
+
+//mute sound function
+function mute_sound(){
+	track.volume = 0;
+	volume.value = 0;
+	volume_show.innerHTML = 0;
+}
+
+
+// checking.. the song is playing or not
+ function justplay(){
+ 	if(Playing_song==false){
+ 		playsong();
+
+ 	}else{
+ 		pausesong();
+ 	}
+ }
+
+
+// reset song slider
+ function reset_slider(){
+ 	slider.value = 0;
+ }
+
+// play song
+function playsong(){
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+}
+
+//pause song
+function pausesong(){
+	track.pause();
+	Playing_song = false;
+	play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+}
+
+
+// next song
+function next_song(){
+	if(index_no < All_song.length - 1){
+		index_no += 1;
+		load_track(index_no);
+		playsong();
+	}else{
+		index_no = 0;
+		load_track(index_no);
+		playsong();
+
+	}
+}
+
+
+// previous song
+function previous_song(){
+	if(index_no > 0){
+		index_no -= 1;
+		load_track(index_no);
+		playsong();
+
+	}else{
+		index_no = All_song.length;
+		load_track(index_no);
+		playsong();
+	}
+}
+
+
+// change volume
+function volume_change(){
+	volume_show.innerHTML = recent_volume.value;
+	track.volume = recent_volume.value / 100;
+}
+
+
+// change slider position 
+function change_duration(){
+	slider_position = track.duration * (slider.value / 100);
+	track.currentTime = slider_position;
+}
+
+
+// autoplay function
+function autoplay_switch(){
+	if (autoplay==1){
+       autoplay = 0;
+       auto_play.style.background = "linear-gradient(145deg, #4d8fcf, #5baaf6)";
+	}else{
+       autoplay = 1;
+       auto_play.style.background = "linear-gradient(145deg, #cf5fb0, #f670d2)";
+	}
+}
+
+
+function range_slider(){
+	let position = 0;
+        
+        // update slider position
+		if(!isNaN(track.duration)){
+		   position = track.currentTime * (100 / track.duration);
+		   slider.value =  position;
+	      }
+
+       
+       // function will run when the song is over
+       if(track.ended){
+       	 play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+           if(autoplay==1){
+		       index_no += 1;
+		       load_track(index_no);
+		       playsong();
+           }
+	    }
+    }

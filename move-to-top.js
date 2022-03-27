eac2493866e0d4b55e5bb405be11a1ddb5f8879d
TG922 <456gaurtarun@gmail.com>
2022-03-27 11:47:23 +0530

    Added Bottom Up top button

---
+++ move-to-top.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+      $('#topBtn').fadeIn();
+    } else{
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function(){
+    $('html ,body').animate({scrollTop : 0},800);
+  });
+});

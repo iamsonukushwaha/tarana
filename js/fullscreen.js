368b92ece8b11a011c6346f4f768221673f13408
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2022-10-09 16:55:21 +0530

    Add files via upload

---
+++ js/fullscreen.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function () {
+    toggleFullScreen();
+
+}, { once: true });
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+}
+
